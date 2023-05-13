a17fd0a070fd717a147d8829ab2540672bf12648
Oscar Shen <oscar_shen@bcit.ca>
2023-05-12 21:10:12 -0700

    sp-metadata.xml

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -13,6 +14,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
 import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider.ResponseToken;
+import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,21 +28,33 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfiguration {
 
+    @Autowired
+    private RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+    
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         OpenSaml4AuthenticationProvider authenticationProvider = new OpenSaml4AuthenticationProvider();
         authenticationProvider.setResponseAuthenticationConverter(groupsConverter());
 
+        DefaultRelyingPartyRegistrationResolver relyingPartyRegistrationResolver = new DefaultRelyingPartyRegistrationResolver(this.relyingPartyRegistrationRepository);
+        Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSamlMetadataResolver());        
+
+     
+
+        
         // @formatter:off
         http
             .authorizeHttpRequests(authorize -> authorize
                 .anyRequest().authenticated()
+                // .anyRequest().permitAll()
             )
             .saml2Login(saml2 -> saml2
                 .authenticationManager(new ProviderManager(authenticationProvider))
             )
-            .saml2Logout(withDefaults());
+            .saml2Logout(withDefaults())
+            .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
+            
         // @formatter:on
 
         return http.build();
